27c9919a6b42c24af6207a665ff30744c28c612a
elen-volodina <jjj@mail.ru>
2025-10-22 21:01:54 +0300

    prebuild go pack tool

--- ynd/go/1.25/ix.sh
+++ ynd/go/1.25/ix.sh
@@ -47,7 +47,7 @@ sha:{{self.archive_hash().strip()}}
 {% endblock %}
 
 {% block step_build %}
-./bin/go build "src/cmd/pack"
+bin/go build "src/cmd/pack"
 sed -i 's/GOTOOLCHAIN=auto/GOTOOLCHAIN=local/g' go.env
 rm -r "test/fixedbugs/issue27836.dir"
 {% endblock %}
